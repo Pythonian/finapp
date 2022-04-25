95da7c12af71aacefb78a126887aacefad22b0fc
Seyi Pythonian <prontomaster@gmail.com>
2022-04-25 06:19:03 +0100

    Finapp Home and Transactions setup

---
+++ finapp/urls.py
@@ -0,0 +1,36 @@
+"""finapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('payments/', views.payments, name='payments'),
+    path('analytics/', views.analytics, name='analytics'),
+    path('goals/', views.goals, name='goals'),
+    path('more/', views.more, name='more'),
+    path('transactions/', views.transactions, name='transactions'),
+    path('transaction_detail/',
+         views.transaction_detail, name='transaction_detail'),
+    path('', views.home, name='home'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ finapp/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def payments(request):
+    return render(request, 'payments.html')
+
+
+def analytics(request):
+    return render(request, 'analytics.html')
+
+
+def goals(request):
+    return render(request, 'goals.html')
+
+
+def more(request):
+    return render(request, 'more.html')
+
+
+def transactions(request):
+    return render(request, 'transactions.html')
+
+
+def transaction_detail(request):
+    return render(request, 'transaction_detail.html')
